94fd9a3dd27b2354766937129d1060af1802f209
Gytopia <dogyt14@gmail.com>
2024-01-22 10:58:30 +0200

    exan

---
+++ Екзамен/exam.py
@@ -0,0 +1,8 @@
+class MyClass:
+    def my_method(self, additional_argument):
+        print("Виклик методу з додатковим аргументом:", additional_argument)
+        return True
+
+my_instance = MyClass()
+result = my_instance.my_method("Додатковий текст")
+print("Результат методу:", result
